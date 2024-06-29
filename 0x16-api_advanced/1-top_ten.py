50885a3f89155d0b0dbbade6ba4cf48dfac38723
Fayemi Funmilayo <busila976@gmail.com>
2024-06-29 05:11:07 +0100

    top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,11 +13,11 @@
                             user = {'User-Agent': 'Lizzie'}
                                 url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
                                                                .format(subreddit), headers=user).json()
-                                    try:
-                                                for post in url.get('data').get('children'):
-                                                                print(post.get('data').get('title'))
-                                                                    except Exception:
-                                                                                print(None)
+                                    try:  
+                                         for post in url.get('data').get('children'):
+                                                    print(post.get('data').get('title'))
+                                                          except Exception:
+                                                                             print(None)
 
 
                                                                                 if __name__ == "__main__":
